36b90717601fba322c3a50533707eeb9a8726c63
Irakli Barbakadze <irbarbaqadze@gmail.com>
2023-06-13 20:50:01 +0400

    add pounds calculation

--- mass-converter.py
+++ mass-converter.py
@@ -8,5 +8,6 @@ amount = float(input("Amount: "))
 if op == "kg":
 
     print(f"Gramm: {kg * 1000}")
-if  op == "g":
+    print(f"Pounds: {amount * 2.2046}")
+elif  op == "g":
     print(f"kg: {amount / 1000}")
