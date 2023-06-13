44b431cbd704bda317dabb5dea0ef9885d2fd22e
Irakli Barbakadze <irbarbaqadze@gmail.com>
2023-06-13 20:28:25 +0400

    Add gram calculation

--- mass-converter.py
+++ mass-converter.py
@@ -1,3 +1,12 @@
-kg = float(input("kg: "))
 
-print(f"Gramm: {kg * 1000}")
+
+op = input("kg/g? ")
+
+amount = float(input("Amount: "))
+
+
+if op == "kg":
+
+    print(f"Gramm: {kg * 1000}")
+if  op == "g":
+    print(f"kg: {amount / 1000}")
